1aaed0fa1c31b8d7e2b4a739b63c3911f33deb87
Jay Watson <watson.jay@outlook.com>
2021-05-02 07:02:42 -0500

    Complete basic app

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getMotivation() {
+    return this._http.get('assets/data/motivation.json');
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
+import { Quote } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private _data: DataService) { }
+
+  motivationQuotes: Array<Quote>;
+  motivationToDisplay: Quote;
+  randomNum: number;
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this._data.getMotivation().subscribe((d: Array<Quote>) => {
+      this.motivationQuotes = d;
+      this.motivationToDisplay = this.motivationQuotes[Math.floor((Math.random() * this.motivationQuotes.length) + 1)];
+    });
+  }
+
+  getNewMotivation() {
+    this.motivationToDisplay = this.motivationQuotes[Math.floor((Math.random() * this.motivationQuotes.length) + 1)];
+  }
 
 }
